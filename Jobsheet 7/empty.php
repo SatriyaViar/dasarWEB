7f0059e4da6b94061e4550fda22edaeab4cd25cb
= <=>
2024-10-08 08:50:49 +0700

    JS 07 - Praktikum 2

---
+++ Jobsheet 7/empty.php
@@ -0,0 +1,9 @@
+<?php
+    $myArray = array();
+    if (empty($nonExistentVar)) {
+        # code...
+        echo "Arrray tidak terdeteksi atau kosong";
+    } else {
+        echo "Array terdefinisi dan tidak kosong";
+    }
+?>
